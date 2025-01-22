dc13551c35ba441c195f6ab5998209fa4c97d713
AAravindDC <Aravind.Arampu@dcmail.ca>
2025-01-22 12:17:14 -0500

    2nd commit

---
+++ new file/src/Main.java
@@ -0,0 +1,18 @@
+package newpackage;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        float math = 98.23f;
+        float english = 87.23f;
+        float science = 85.42f;
+        float average = (math + english + science)/3f;
+
+        System.out.println("My mark for Math is "+ math+"%");
+        System.out.println("My mark for English is "+ english+"%");
+        System.out.println("My mark for Science is "+ science+"%");
+
+        }
+    }
